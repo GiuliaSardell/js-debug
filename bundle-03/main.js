23af3f4d7d118ec231ab3e76992419b8146d44cd
Giulia Sardell <giuliasardelli1234@gmail.com>
2021-12-09 18:09:35 +0100

    bundle 3

--- bundle-03/main.js
+++ bundle-03/main.js
@@ -56,31 +56,44 @@ const cars = [
         manufacturer: 'Seat',
         model: 'Ibiza',
         type: 'metano'
-    }
+    },
     {
         manufacturer: 'Audi',
         model: 'R8',
         type: 'Benzina'
     },
-];
+  ];
+  
+  const gasolineCars = cars.filter( (auto) => auto.type.toLocaleUpperCase() == 'benzina'.toLocaleUpperCase());
+  
+  const dieselCars = cars.filter( (auto) => auto.type.toLocaleUpperCase() == 'diesel'.toLocaleUpperCase());
+  
+  const otherCars = cars.filter( (auto) => auto.type.toLocaleUpperCase() != 'benzina'.toLocaleUpperCase() && auto.type.toLocaleUpperCase() != 'diesel'.toLocaleUpperCase());
+  
+  console.log('Auto a benzina');
+  console.log('*******************************');
+  console.log(gasolineCars);
+  
+  console.log('Auto a diesel');
+  console.log('*******************************');
+  console.log(dieselCars);
+  
+  console.log('Tutte le altre auto');
+  console.log(otherCars);
 
-const gasolineCars = cars.filter( (auto) >= auto.type === 'benzina');
-
-const dieselCars = cars.filter( (auto) => {
-    auto.type === 'diesel';
-});
-
-const otherCars = cars.filter( (auto) => {
-    return auto.type !== 'benzina' || auto.type !== 'diesel';
-});
-
-console.log('Auto a benzina');
-console.log('*******************************');
-console.log(gasolineCars);
-
-console.log('Auto a diesel');
-console.log('*******************************');
-console.log(dieselCars);
-
-console.log('Tutte le altre auto');
-console.log(otherCars);
+/*******************************************************************************
+​
+    Per questo esercizio, rispondi a queste domande:
+    1- Che cosa fa questo codice?
+    dato un array di oggetti con dati di macchine, dividere le macchine in array diversi in base al loro type
+    2- Sono presenti errori di sintassi?
+    manca una virgola tra un oggetto e l'altro
+    le arrow function sono scritte sbagliate
+    va aggiunto il metodo .toLocaleUpperCase() 
+    bisogna mettere == e non ===
+    nella terza arrow function va messo && e non || 
+    3- Sono presenti errori logici?
+​
+    Inserisci i tuoi commenti con le risposte alla fine dell'esercizio
+​
+*******************************************************************************/
